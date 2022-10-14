9653e7a636963dec1252c2198d1aff1ca1e6d6a0
Paul <pacalabre@gmail.com>
2022-10-14 16:33:58 -0600

    Added recipe routes and validation.

--- models/recipes.js
+++ models/recipes.js
@@ -21,6 +21,8 @@ const recipeSchema = new mongoose.Schema({
   author: {
     type: String,
     required: true,
+    minlength: 1,
+    maxlength: 85,
   },
   favorites: [String],
   dateCreated: {
@@ -28,7 +30,7 @@ const recipeSchema = new mongoose.Schema({
     required: true,
     default: Date.now,
   },
-  updatedDate: Date,
+  updatedOnDate: Date,
   tags: [String],
   link: String,
   easeOfMaking: {
@@ -53,5 +55,27 @@ const recipeSchema = new mongoose.Schema({
 
 const Recipe = new mongoose.model("Recipe", recipeSchema);
 
+function validateRecipe(userRequest) {
+  const schema = Joi.object({
+    id: Joi.string(),
+    title: Joi.string().min(1).max(50).required(),
+    subtitle: Joi.string().min(1).max(280).required(),
+    image: Joi.string(),
+    author: Joi.string().min(1).max(85).required(),
+    favorites: Joi.array(),
+    dateCreated: Joi.date(),
+    updatedOnDate: Joi.date(),
+    tags: Joi.array(),
+    link: Joi.string(),
+    easeOfMaking: Joi.number().min(1).max(5),
+    totalMakeTime: Joi.string(),
+    ingredients: Joi.array().required(),
+    description: Joi.string().min(1).max(5000),
+    steps: Joi.array().required(),
+  });
+  return schema.validate(userRequest);
+}
+
 exports.Recipe = Recipe;
+exports.validateRecipe = validateRecipe;
 exports.recipeSchema = recipeSchema;
--- routes/recipes.js
+++ routes/recipes.js
@@ -2,14 +2,97 @@ const express = require("express");
 const router = express.Router();
 const { Recipe, validateRecipe } = require("../models/recipes");
 
-//get all
+router.get("/", async (req, res) => {
+  try {
+    const recipe = await Recipe.find();
+    res.send(recipe);
+  } catch (error) {
+    res.send(`There was an error: ${error}`);
+  }
+});
 
-//get specific recipe
+router.get("/:id", async (req, res) => {
+  try {
+    const recipe = await Recipe.findById(req.params.id);
+    if (!recipe) {
+      res.status(404).send("Recipe not found");
+    }
+    res.send(recipe);
+  } catch (error) {
+    res.send(`There was an error ${error}`);
+  }
+});
 
-//post
+router.post("/", async (req, res) => {
+  const { error } = validateRecipe(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  try {
+    const newRecipe = new Recipe({
+      title: req.body.title,
+      subtitle: req.body.subtitle,
+      image: req.body.image,
+      author: req.body.author,
+      favorites: req.body.favorites,
+      dateCreated: req.body.dateCreated,
+      updatedOnDate: req.body.updatedOnDate,
+      tags: req.body.tags,
+      link: req.body.link,
+      easeOfMaking: req.body.easeOfMaking,
+      totalMakeTime: req.body.totalMakeTime,
+      ingredients: req.body.ingredients,
+      description: req.body.description,
+      steps: req.body.steps,
+    });
+    newSavedRecipe = await newRecipe.save();
+    res.send(newSavedRecipe);
+  } catch (error) {
+    res.send(`There was an error: ${error}`);
+  }
+});
 
-//update
+router.put("/:id", async (req, res) => {
+  const { error } = validateRecipe(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  try {
+    const recipe = await Recipe.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      subtitle: req.body.subtitle,
+      image: req.body.image,
+      author: req.body.author,
+      favorites: req.body.favorites,
+      dateCreated: req.body.dateCreated,
+      updatedOnDate: req.body.updatedOnDate,
+      tags: req.body.tags,
+      link: req.body.link,
+      easeOfMaking: req.body.easeOfMaking,
+      totalMakeTime: req.body.totalMakeTime,
+      ingredients: req.body.ingredients,
+      description: req.body.description,
+      steps: req.body.steps,
+    });
+    if (!recipe) {
+      res.status(404).send("Recipe not found");
+    }
+    res.send(recipe);
+  } catch (error) {
+    res.send(`There was an error: ${error}`);
+  }
+});
 
-//delete
+router.delete("/:id", async (req, res) => {
+  try {
+    const recipe = await Recipe.findByIdAndRemove(req.params.id);
+    if (!recipe) {
+      res.status(404).send("Recipe not found");
+    }
+    res.send(recipe);
+  } catch (error) {
+    res.send(`There was an error ${error}`);
+  }
+});
 
 module.exports = router;
