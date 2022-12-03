dec461374562b6afa1ea6044f506a52b769bf539
Paul <pacalabre@gmail.com>
2022-12-03 16:47:33 -0700

    Added initial login and logout functionality.

---
+++ passportConfig.js
@@ -0,0 +1,52 @@
+const { User } = require("./models/users");
+const bcrypt = require("bcrypt");
+const localStrategy = require("passport-local").Strategy;
+
+module.exports = function (passport) {
+  passport.use(
+    new localStrategy(
+      { usernameField: "email", passwordField: "password" },
+      (email, password, done) => {
+        User.findOne({ email: email }, (err, user) => {
+          if (err) throw err;
+          if (!user) return done(null, false);
+          bcrypt.compare(password, user.password, (err, result) => {
+            if (err) throw err;
+            if (result === true) {
+              return done(null, user);
+            } else {
+              return done(null, false);
+            }
+          });
+        });
+      }
+    )
+  );
+
+  passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+  });
+  passport.deserializeUser((id, cb) => {
+    User.findOne({ _id: id }, (err, user) => {
+      const userInformation = {
+        username: user.username,
+      };
+      cb(err, userInformation);
+    });
+  });
+};
+
+//middleware
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  else {
+    console.log("USER IS NOT AUTHENTICATED");
+  }
+}
+
+function isLoggedOut(req, res, next) {
+  if (!req.isAuthenticated()) return next();
+  else {
+    console.log("USER IS NOT AUTHENTICATED");
+  }
+}
