9b409f9b2efc8bf0fea31a5a04716e8e0570677a
Paul Calabrese <pacalabre@gmail.com>
2025-06-11 11:09:53 -0600

    Adding maxAge.

--- startup/routes.js
+++ startup/routes.js
@@ -20,6 +20,9 @@ module.exports = function (app) {
       secret: `${process.env.SESSION_SECRET}`,
       resave: false,
       saveUninitialized: false,
+      cookie: {
+        maxAge: 60 * 60 * 1000,
+      },
       store: MongoStore.create({
         mongoUrl: `${process.env.MONGO_URI}/caladine`,
         ttl: 604800,
