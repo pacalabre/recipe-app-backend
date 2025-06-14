c993231539b77953358d217f3e51ae5f6eddb4d1
Paul Calabrese <pacalabre@gmail.com>
2025-06-14 13:10:03 -0600

    Logging data when getting user

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,7 @@ const Joi = require("joi");
 
 router.get("/user", (req, res) => {
   if (!req.user) res.status(404).send("There is no user currently logged in.");
-  console.log("session user", req.session.passport.user);
+  console.log("session user", req.session);
   const userToSend = {
     id: req.user.id,
     name: req.user.name,
